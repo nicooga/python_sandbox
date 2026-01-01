e21cd992bb1cee7eddc83bddebd17acc46ca219d
Nicolas Oga <2112.oga@gmail.com>
2026-01-01 21:41:01 +0000

    Enrich frech_deck.py with comments

--- pythons_data_model/french_deck.py
+++ pythons_data_model/french_deck.py
@@ -13,8 +13,14 @@ class FrenchDeck:
             for suit in self.suits
         ]
 
+    # Special "dunder" methods are methods that are called when the object is used in a special way.
+    # "dunder" means "double underscore before and after".
+    # For instance, __len__ is called when the object is used in a len() function.
+    # Usage: len(deck)
     def __len__(self):
         return len(self._cards)
 
+    # __getitem__ is called when the object is used in a [] operator.
+    # Usage: deck[0]
     def __getitem__(self, position):
         return self._cards[position]
