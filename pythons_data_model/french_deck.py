cbcde4e79c3fe4f0f537387b6a3475ba51c2ff84
Nicolas Oga <2112.oga@gmail.com>
2026-01-01 21:39:23 +0000

    Introduce examples for data model in french_deck.py

---
+++ pythons_data_model/french_deck.py
@@ -0,0 +1,20 @@
+from collections import namedtuple
+
+Card = namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list[str]('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [
+            Card(rank, suit)
+            for rank in self.ranks
+            for suit in self.suits
+        ]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
